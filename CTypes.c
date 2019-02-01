0d12fc44152b8d6cf6ad4e303d23848d0249696f
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-01-31 22:51:45 -0200

    Update CTypes.c
    Add "Default settings compilation" code.

--- CTypes.c
+++ CTypes.c
@@ -89,7 +89,73 @@ int main(int argc, char *argv[])
 
 	printf("double size: %ld bytes. Limits: [%.20le, %.20le]. Number of digits in the number: [%d]\n\n", sizeof(double), DBL_MIN, DBL_MAX, DBL_MANT_DIG);
 
-	printf("long double size: %ld bytes. Limits: [%.20Le, %.20Le]. Number of digits in the number: [%d]\n", sizeof(long double), LDBL_MIN, LDBL_MAX, LDBL_MANT_DIG);
+	printf("long double size: %ld bytes. Limits: [%.20Le, %.20Le]. Number of digits in the number: [%d]\n\n", sizeof(long double), LDBL_MIN, LDBL_MAX, LDBL_MANT_DIG);
 
+	printf("Default settings compilation (see feature_test_macros(7)):\n");
+	
+	#ifdef _POSIX_SOURCE
+		printf("_POSIX_SOURCE defined\n");
+	#endif
+
+	#ifdef _POSIX_C_SOURCE
+		printf("_POSIX_C_SOURCE defined: %ldL\n", (long) _POSIX_C_SOURCE);
+	#endif
+
+	#ifdef _ISOC99_SOURCE
+		printf("_ISOC99_SOURCE defined\n");
+	#endif
+
+	#ifdef _ISOC11_SOURCE
+		printf("_ISOC11_SOURCE defined\n");
+	#endif
+
+	#ifdef _XOPEN_SOURCE
+		printf("_XOPEN_SOURCE defined: %d\n", _XOPEN_SOURCE);
+	#endif
+
+	#ifdef _XOPEN_SOURCE_EXTENDED
+		printf("_XOPEN_SOURCE_EXTENDED defined\n");
+	#endif
+
+	#ifdef _LARGEFILE64_SOURCE
+		printf("_LARGEFILE64_SOURCE defined\n");
+	#endif
+
+	#ifdef _FILE_OFFSET_BITS
+		printf("_FILE_OFFSET_BITS defined: %d\n", _FILE_OFFSET_BITS); /* api de LOG !!!!! */
+	#endif
+
+	#ifdef _BSD_SOURCE
+		printf("_BSD_SOURCE defined\n");
+	#endif
+
+	#ifdef _SVID_SOURCE
+		printf("_SVID_SOURCE defined\n");
+	#endif
+
+	#ifdef _DEFAULT_SOURCE
+		printf("_DEFAULT_SOURCE defined\n");
+	#endif
+
+	#ifdef _ATFILE_SOURCE
+		printf("_ATFILE_SOURCE defined\n");
+	#endif
+
+	#ifdef _GNU_SOURCE
+		printf("_GNU_SOURCE defined\n");
+	#endif
+
+	#ifdef _REENTRANT
+		printf("_REENTRANT defined\n");
+	#endif
+
+	#ifdef _THREAD_SAFE
+		printf("_THREAD_SAFE defined\n");
+	#endif
+
+	#ifdef _FORTIFY_SOURCE
+		printf("_FORTIFY_SOURCE defined\n");
+	#endif
+	
 	return(0);
 }
