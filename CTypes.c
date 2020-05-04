6fee2923d9cc5607c7a76ae7e86e21535a00c79f
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-05-04 01:15:24 -0300

    Update CTypes.c

--- CTypes.c
+++ CTypes.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <stdint.h>
 #include <time.h>
 #include <unistd.h>
 #include <float.h>
 #include <sys/types.h>
-
 
 /* ver tb:
  * getrlimit()
